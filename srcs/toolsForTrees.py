baf0d02a1a1807728e8706e7204115951f6914f2
Emmanuelle TERMEAU <emtermea@e2r4p12.42.fr>
2017-05-10 19:15:32 +0200

    fonction pour check le type des noeuds

--- srcs/toolsForTrees.py
+++ srcs/toolsForTrees.py
@@ -12,3 +12,26 @@ def printValue(node):
 	except:
 		print node.value.letter
 		print node.value.value
+
+def checkTypeNode(node):
+	if node.left:
+		checkTypeNode(node.left)
+	if node.right:
+		checkTypeNode(node.right)
+	try:
+		return node.value.typeOp
+	except:
+		if not node.value.letter:
+			checkTypeNode(node)
+		else:
+			return node.value.letter
+
+# def ApplyRuleOnNode(node):
+# 	if node.left:
+# 		ApplyRuleOnNode(node.left)
+# 	if node.right:
+# 		ApplyRuleOnNode(node.right)
+# 	try:
+# 		return node.value.typeOp.apply(node.left, node.right)
+# 	except:
+# 		pass
