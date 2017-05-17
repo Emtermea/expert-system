b31133323c0d25ff5b98882aec543f9206c1e0fa
Emmanuelle Termeau <emtermea@student.42.fr>
2017-05-17 17:50:21 +0200

    pseudo code pour l'algo de resolution

--- srcs/resolv.py
+++ srcs/resolv.py
@@ -10,8 +10,8 @@ from srcs.OnlyIf import OnlyIf
 from srcs.Rules import Rules
 from srcs.toolsForTrees import *
 
-def applyRule(rule, ValueInNode): #appliquer la regle designee comme applicable
-	pass
+# def applyRule(rule, ValueInNode): #appliquer la regle designee comme applicable
+# 	pass
 
 def checkRelevantRule(rule, factsBase): #check si la regle est applicable
 	lettersInRule = []
@@ -33,11 +33,29 @@ def findRelevantRule(rulesBase, factsBase): # trouver la premier regle applicabl
 			return rule
 	return 0
 
+def resolvExp(exp):
+	# apply le retour de RelevantRule
+	# while il y a des expresions dans l'expleft
+		# applyInExp de lexpresion de // pour gerer les parantheses
+	# applyInExp(expleft)
+	# if applyInExp(expright) == letter
+		# la lettre de l'expright == le retour de applyInExp(left)
+
+
+def applyInExp(exp, factsBase):
+	# trouver le noeud de l'exp
+	# if (il y a un typeOp):
+	# apply en fonctiono du typeOP avec node.left et node.right
+	# return la valeur
+	# else:
+	# return la value de la letter
+
 def resolv(factsBase, rulesBase, letter): # fonction de resolution
 	RelevantRule = findRelevantRule(rulesBase, factsBase)
 	return RelevantRule
 	# while (letter not in factsBase) and RelevantRule:
 		# apply le retour de RelevantRule
+		# resolvExp
 		# push le resultat de apply dans factsBase
 		# desactivation de la regle:
 		# rulesBase.remove(RelevantRule)
